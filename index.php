b71dc0b3e08506ac4279aa7f761a9b0cf8515ae3
blitzkrieg1309 <al.fatihnomer1@gmail.com>
2024-11-21 15:37:37 +0700

    index.php | img folder
    Update index.php and add Heroindex.jpg img folder

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -9,12 +8,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
   </head>
   <body>
-    <div class="container item-center position-absolute top-50 start-0 translate-middle-y">
-      <div class="d-flex flex-row">
-        <div class="col-8"><img class="align-item-center" src="" alt="test" /></div>
-        <div class="col-4">
+    <div class="container d-flex flex-column align-items-center justify-content-center vh-100">
+      <div class="row">
+        <div class="col-md-8 col-12 d-flex justify-content-center">
+          <img class="img-fluid rounded shadow" src="img/Heroindex.jpg" alt="" style="width: 90%;" />
+        </div>
+        <div class="col-md-4 col-12 d-flex flex-column ">
           <form action="">
-            <h2 class="text-center mb-5">Login</h2>
+            <h2 class="text-center mt-5 mb-5">Login</h2>
             <label for="username" class="form-label">Username :</label>
             <input class="form-control" type="username" type="text" id="username" name="username" placeholder="Enter your username" />
 
@@ -26,6 +27,6 @@
         </div>
       </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3. 3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
